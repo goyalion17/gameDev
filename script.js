3e0920dcf94bfb40e07009845335bf6ce9be9dd2
Irina Saveleva <goyalion17@gmail.com>
2023-03-15 22:03:08 +0100

    Larva sprites and collisions

--- script.js
+++ script.js
@@ -200,9 +200,11 @@ window.addEventListener('load', function() {
             this.spriteX
             this.spriteY
             this.speedY = 1 + Math.random()
+            this.frameX = 0
+            this.frameY = Math.floor(Math.random() * 2)
         }
         draw(context) {
-            context.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.spriteX, this.spriteY, this.width, this.height)
+            context.drawImage(this.image, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, this.spriteWidth, this.spriteHeight, this.spriteX, this.spriteY, this.width, this.height)
             if (this.game.debug) {
                 context.beginPath();
                 context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2);
@@ -221,7 +223,27 @@ window.addEventListener('load', function() {
             if (this.collisionY < this.game.topMargin) {
                 this.markedForDeletion = true
                 this.game.removeGameObjects()
+                this.game.score++
             }
+            // collision with objects
+            let collisionObject = [this.game.player, ...this.game.obstacles]
+            collisionObject.forEach(object => {
+                let [collision, distance, sumOfRadii, dx, dy] = this.game.checkCollision(this, object)
+                if (collision) {
+                    const unit_x = dx / distance
+                    const unit_y = dy / distance
+                    this.collisionX = object.collisionX + (sumOfRadii + 1) * unit_x
+                    this.collisionY = object.collisionY + (sumOfRadii + 1) * unit_y
+                }
+            })
+            // collision with enemies
+            this.game.enemies.forEach(enemy => {
+                if (this.game.checkCollision(this, enemy)[0]) {
+                    this.markedForDeletion = true
+                    this.game.removeGameObjects()
+                    this.game.lostHatchlings++
+                }
+            })
         }
     }
 
@@ -293,6 +315,8 @@ window.addEventListener('load', function() {
             this.enemies = []
             this.hatchlings = []
             this.gameObject = []
+            this.score = 0
+            this.lostHatchlings = 0
             this.mouse = {
                 x: this.width * 0.5,
                 y: this.height * 0.5,
